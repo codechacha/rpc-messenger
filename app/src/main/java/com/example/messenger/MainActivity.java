046cc906ae7dfce660672d870d024320e4b94026
mj <farfs.dev@gmail.com>
2017-05-07 18:35:33 +0900

    first commit

---
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.messenger;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+
+    private Messenger mServiceCallback = null;
+    private Messenger mClientCallback = new Messenger(new CallbackHandler());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAddValue = (Button) findViewById(R.id.btn_add_value);
+        btnAddValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceCallback != null) {
+                    // request 'add value' to service
+                    Message msg = Message.obtain(null, RemoteService.MSG_ADD_VALUE);
+                    msg.arg1 = 10;
+                    msg.replyTo = mClientCallback;
+                    try {
+                        mServiceCallback.send(msg);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(TAG, "Send MSG_ADD_VALUE message to Service");
+                }
+            }
+        });
+
+        Log.d(TAG, "Trying to connect to service");
+        Intent intent = new Intent(getApplicationContext(), RemoteService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    private ServiceConnection mConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "onServiceConnected");
+            mServiceCallback = new Messenger(service);
+
+            // connect to service
+            Message connect_msg = Message.obtain( null, RemoteService.MSG_CLIENT_CONNECT);
+            connect_msg.replyTo = mClientCallback;
+            try {
+                mServiceCallback.send(connect_msg);
+                Log.d(TAG, "Send MSG_CLIENT_CONNECT message to Service");
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "onServiceDisconnected");
+            mServiceCallback = null;
+        }
+    };
+
+    private class CallbackHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case RemoteService.MSG_ADDED_VALUE:
+                    Log.d(TAG, "Recevied MSG_ADDED_VALUE message from service ~ value :" + msg.arg1);
+                    break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/messenger/RemoteService.java
@@ -0,0 +1,86 @@
+package com.example.messenger;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mjs on 2017-03-04.
+ */
+
+public class RemoteService extends Service{
+    private final String TAG = "RemoteService";
+    public static final int MSG_CLIENT_CONNECT = 1;
+    public static final int MSG_CLIENT_DISCONNECT = 2;
+    public static final int MSG_ADD_VALUE = 3;
+    public static final int MSG_ADDED_VALUE = 4;
+
+    private ArrayList<Messenger> mClientCallbacks = new ArrayList<Messenger>();
+    final Messenger mMessenger = new Messenger( new CallbackHandler());
+    int mValue = 0;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
+    private class CallbackHandler  extends Handler {
+        @Override
+        public void handleMessage( Message msg ){
+            switch( msg.what ){
+                case MSG_CLIENT_CONNECT:
+                    Log.d(TAG, "Received MSG_CLIENT_CONNECT message from client");
+                    mClientCallbacks.add(msg.replyTo);
+                    break;
+                case MSG_CLIENT_DISCONNECT:
+                    Log.d(TAG, "Received MSG_CLIENT_DISCONNECT message from client");
+                    mClientCallbacks.remove(msg.replyTo);
+                    break;
+                case MSG_ADD_VALUE:
+                    Log.d(TAG, "Received message from client: MSG_ADD_VALUE");
+                    mValue += msg.arg1;
+                    for (int i = mClientCallbacks.size() - 1; i >= 0; i--) {
+                        try{
+                            Log.d(TAG, "Send MSG_ADDED_VALUE message to client");
+                            Message added_msg = Message.obtain(null, RemoteService.MSG_ADDED_VALUE);
+                            added_msg.arg1 = mValue;
+                            mClientCallbacks.get(i).send(added_msg);
+                        }
+                        catch( RemoteException e){
+                            mClientCallbacks.remove( i );
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+
+}
